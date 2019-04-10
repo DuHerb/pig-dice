3d25c515159daf3ce352f06c90a59a4e8f6bf9df
Dustin Herboldshimer and Marc Davies <dustin@>
2019-04-10 15:04:47 -0700

    add function to change active status for next player in turn

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,35 @@
+function Game(){
+  this.currentId = -1,
+  this.players = [];
+}
+
+Game.prototype.addNewPlayer = function(player) {
+  this.currentId ++;
+  player.playerId = this.currentId;
+  this.players.push(player);
+}
+
+Game.prototype.changeStatus = function() {
+  var length = this.players.length;
+  console.log(this.players);
+  console.log(this.players[0].status);
+  console.log(length);
+    if(this.players[length - 1 ].status === true) {
+      this.players[length - 1].status = false;
+      this.players[0].status = true;
+    } else {
+
+      for(var i=0; i < length; i++){
+        if (this.players[i].status === true){
+          this.players[i].status = false;
+          this.players[i + 1].status = true;
+          break;
+      };
+    };
+  }
+}
+
+
 // Busi
   function Player(playerName) {
     this.playerId = 0,
@@ -5,7 +37,7 @@
     this.diceRoll = 0,
     this.turnScore = 0,
     this.totalScore = 0,
-    this.status = true
+    this.status = false
   }
 
   // Player.prototype.turn = function(){
@@ -45,12 +77,28 @@
     };
   }
 
-var player1 = new Player("Dino");
+
+
+var player1 = new Player("Dustin");
+var player2 = new Player("Marc");
+// var player3 = new Player("admin");
 
 
 // on rollDiceClick
 
 $(function() {
+  var game = new Game();
+  game.addNewPlayer(player1);
+  game.addNewPlayer(player2);
+  // game.addNewPlayer(player3);
+  player1.status = true;
+  // console.log(player1.status);
+  // console.log(player2.status);
+  // console.log(player3.status);
+  // game.changeStatus();
+  // console.log(player1.status);
+  // console.log(player2.status);
+  // console.log(player3.status);
   $(".rollDice").on("click", function(event){
     // event.StopImmediatePropagation();
     // event.StopPropagation();
