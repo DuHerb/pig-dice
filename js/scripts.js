56dc46ddf6f9635fe401ff9d23801f386eb96b3a
Dustin Herboldshimer and Marc Davies <marc@>
2019-04-10 10:15:00 -0700

    add rollDice()

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,18 @@
 $(function() {
 
+  // player logic
+  function Player() {
+    this.playerId = 0,
+    this.totalScore = 0
+  }
+
+  function rollDice(){
+    return Math.floor((Math.random() * 6) +1);
+  }
+
+// on rollDiceClick
+  $('#rollDice').on("click", function(){
+    var roll = rollDice();
+    $('#diceScore').text(roll);
+  })
 });
