742bff5e92d445546e4217107bce29893301da34
Dustin Herboldshimer and Marc Davies <dustin@>
2019-04-10 13:42:33 -0700

    replace hold() with endTurn()

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,48 @@
-
-
-
-
-  // player logic
+// Busi
   function Player(playerName) {
     this.playerId = 0,
     this.playerName = playerName,
     this.diceRoll = 0,
     this.turnScore = 0,
-    this.totalScore = 0
+    this.totalScore = 0,
+    this.status = true
   }
+
+  // Player.prototype.turn = function(){
+  //   var turnScore = 0;
+  //   var roll =  Math.floor((Math.random() * 6) +1);
+  //   if(roll === 1) {
+  //
+  //   }
+  // }
 
   Player.prototype.rollDice = function() {
     var roll =  Math.floor((Math.random() * 6) +1);
     if (roll === 1) {
       this.diceRoll = 1;
       this.turnScore = 0;
+      alert("you rolled a one. Turn Over")
+      this.endTurn();
     } else {
       this.diceRoll = roll;
       this.turnScore += roll;
     };
   }
 
-  Player.prototype.hold = function() {
+  Player.prototype.endTurn = function(){
     this.totalScore += this.turnScore;
     this.turnScore = 0;
+    this.status = false;
+    // alert("turn is over");
+    this.scoreCheck();
+  }
+
+  Player.prototype.scoreCheck = function() {
+    if (this.totalScore >= 20) {
+      alert("Winner Winner Bacon Dinner");
+      this.status = false;
+      //playAgain = true
+    };
   }
 
 var player1 = new Player("Dino");
@@ -44,7 +62,7 @@ $(function() {
   $(".hold").on( "click", function(event){
     // event.StopImmediatePropagation();
     // event.StopPropagation();
-    player1.hold();
+    player1.endTurn();
     $(".totalScore").text(player1.totalScore);
   })
 });
